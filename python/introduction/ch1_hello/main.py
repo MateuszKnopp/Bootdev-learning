88205c0c8a41778bb5be6cfcb33b48634318c103
mateusz <knoppmateusz@gmail.com>
2025-11-29 23:48:57 +0100

    feat: print() statement

--- python/introduction/ch1_hello/main.py
+++ python/introduction/ch1_hello/main.py
@@ -1,1 +1,6 @@
-print("Welcome to Fantasy Quest!")
+import lib  # <-- To wymusi kompilację lib.py print(lib.greet())
+
+# <-- Pokaz mi czym jest lib.greet (funkcja w pamieci ram o adresie...)
+print(lib.greet)
+# wywolaj funkcje greet()
+print(lib.greet())
---
+++ python/introduction/syntax_error/main.py
@@ -0,0 +1,4 @@
+# "Syntax" is jargon for "valid code that the computer can understand". For example, the following code has invalid syntax:
+
+# print("hello world')
+print("hello world!")
