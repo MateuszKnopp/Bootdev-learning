6eaee4ac87859b5caf74522e2333eddf26fb729d
mateusz <knoppmateusz@gmail.com>
2025-12-01 22:57:15 +0100

    feat: Rozpoczecie rozdzialu o zmiennych

---
+++ python/variables/1_variables/main.py
@@ -0,0 +1,24 @@
+# Variables are how we store data as our program runs.
+# Up 'till now we've been printing data by passing it straight into print(). \
+# Now we're going to save the data in variables so we can reuse it and change it before printing it.
+my_height = 100
+my_name = "Mateusz"
+
+player_health = 1000
+print(player_health)
+
+# reduce by 100 here
+player_health = player_health - 100
+print(player_health)
+
+# and here
+player_health = player_health - 100
+print(player_health)
+
+# and here
+player_health = player_health - 100
+print(player_health)
+
+# and here
+player_health = player_health - 100
+print(player_health)
