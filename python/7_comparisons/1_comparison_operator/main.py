7fef0e7f2b30f826d46ce10d35d9958a6dcd6d60
mateusz <knoppmateusz@gmail.com>
2025-12-06 00:22:10 +0100

    feat: Rozpoczecie rodzialu na temat porownywania wartosci

---
+++ python/7_comparisons/1_comparison_operator/main.py
@@ -0,0 +1,25 @@
+# When coding it's necessary to be able to compare two values.
+# Boolean logic is the name for these kinds of comparison operations that always result in True or False.
+
+# The operators:
+
+# < "less than"
+# > "greater than"
+# <= "less than or equal to"
+# >= "greater than or equal to"
+# == "equal to"
+#!= "not equal to"
+# For example:
+
+5 < 6  # evaluates to True
+5 > 6  # evaluates to False
+5 >= 6  # evaluates to False
+5 <= 6  # evaluates to True
+5 == 6  # evaluates to False
+5 != 6  # evaluates to True
+######################################################################
+
+
+def player_1_wins(player_1_score, player_2_score):
+    return player_1_score > player_2_score
+    pass
---
+++ python/7_comparisons/1_comparison_operator/main2.py
@@ -0,0 +1,6 @@
+def compare_heights(edward_height, alphonse_height, winry_height, mustang_height):
+    isMustangEdwardSame = mustang_height == edward_height
+    isAlphonseEdwardSame = alphonse_height == edward_height
+    isWinryAlphonseSame = winry_height == alphonse_height
+    return isMustangEdwardSame, isAlphonseEdwardSame, isWinryAlphonseSame
+    pass
